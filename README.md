815e9932165f131f37368df154e6c0dd962bd8e9
Mario Gómez <60667991+mgomezarr@users.noreply.github.com>
2023-11-15 15:30:56 -0500

    First functional version of Cube

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-### Spinning Cube
+# Spinning Cube
 
 Esto representa el proceso continuo de cálculo de la matriz de rotación 3D de un cubo.
 
@@ -9,3 +9,28 @@ Con base en la operación de álgebra matricial:
 Dando como resultado la operación:
 
 ![Result](assets/result%20matrix.png)
+
+## Z-Buffer
+
+![Z-Buffer](assets/z-buffer.png)
+En computación gráfica, el z-buffering es la parte de la memoria de un adaptador de video encargada de gestionar las coordenadas de profundidad de las imágenes en los gráficos en tres dimensiones (3-D), normalmente calculados por hardware y algunas veces por software.
+
+El Z-buffering también se conoce como buffering de profundidad, debido a que la profundidad del píxel generado (coordenada z) al ser dibujado por una tarjeta gráfica 3D, se almacena en un búfer de datos (el z-buffer).
+
+---
+
+## memset() - C Library Function
+
+#### `void *memset ( void *s, int c, size_t n);`
+
+La función memset copia el valor de c (convertido a un unsigned char) a cada uno de los primeros n caracteres en el objeto al cual señala s. La función memset devuelve el valor de s.
+
+`memset(buffer, background_ASCII_code, WIDTH * HEIGHT);`
+
+Links: https://www.geeksforgeeks.org/memset-in-cpp/
+
+---
+
+## TODO:
+
+1. Replacing memset() with std::fill();
--- cube.cpp
+++ cube.cpp
@@ -1,33 +1,109 @@
 #include <iostream>
-#include <string>
+#include <cstring>
 #include <cmath>
 #include <unistd.h>
 
-// This represents the rotation of the axes θx, θy, θz.
-float A, B, C;
+using namespace std;
 
-float calculateX(int i, int j, int k)
+// This represents the rotation of the axes θx, θy, θz.
+double A, B, C;
+
+// Vectors
+double x, y, z;
+double ooz;     // ??????????????
+double K1 = 40; // ??????????????
+int xp, yp;     // ??????????????
+int idx;        // ??????????????
+
+// Dimensions
+const int WIDTH = 75, HEIGHT = 30;
+const double increment_speed = 0.5;
+const int distanceFromCam = 50;
+
+// Cube width
+double cube_width = 10;
+
+// Z-buffering
+double z_buffer[WIDTH * HEIGHT];
+
+// Buffer of characters?
+char buffer[WIDTH * HEIGHT];
+
+// Background ASCII code
+const char background_ASCII_code = ' ';
+
+// Calculate vectors of rotation matrices for each axis.
+double calculateX(int i, int j, int k)
 {
   return j * sin(A) * sin(B) * cos(C) - k * cos(A) * sin(B) * cos(C) +
          j * cos(A) * sin(C) + k * sin(A) * sin(C) + i * cos(B) * cos(C);
 }
 
-float calculateY(int i, int j, int k)
+double calculateY(int i, int j, int k)
 {
   return j * cos(A) * cos(C) + k * sin(A) * cos(C) -
          j * sin(A) * sin(B) * sin(C) + k * cos(A) * sin(B) * sin(C) -
          i * cos(B) * sin(C);
 }
 
-float calculateZ(int i, int j, int k)
+double calculateZ(int i, int j, int k)
 {
   return k * cos(A) * cos(B) - j * sin(A) * cos(B) + i * sin(B);
+}
+
+void *calculateForSurface(double cube_x, double cube_y, double cube_z, int ch)
+{
+  x = calculateX(cube_x, cube_y, cube_z);
+  y = calculateY(cube_x, cube_y, cube_z);
+  z = calculateZ(cube_x, cube_y, cube_z) + distanceFromCam;
+
+  ooz = 1 / z;
+  xp = (int)(WIDTH / 2 + K1 * ooz * x * 2);
+  yp = (int)(HEIGHT / 2 + K1 * ooz * y);
+
+  idx = xp + yp * WIDTH;
+  if (idx >= 0 && idx < WIDTH * HEIGHT)
+  {
+    if (ooz > z_buffer[idx])
+    {
+      z_buffer[idx] = ooz;
+      buffer[idx] = ch;
+    }
+  }
 }
 
 int main(int argc, char const *argv[])
 {
   // Cleaning screen like C
   printf("\x1b[2J");
-  /* code */
+
+  // Infinite loop
+  while (1)
+  {
+    // Copying ASCII code into buffer. Size is WIDTH * HEIGHT
+    memset(buffer, background_ASCII_code, WIDTH * HEIGHT);
+
+    // [?] What is this? 4 depth levels?
+    memset(z_buffer, 0, WIDTH * HEIGHT * 4);
+
+    // Looping through X
+    for (double cube_x = -cube_width; cube_x < cube_width; cube_x += increment_speed)
+    {
+      // Looping through Y
+      for (double cube_y = -cube_width; cube_y < cube_width; cube_y += increment_speed)
+      {
+        calculateForSurface(cube_x, cube_y, -cube_width, '.');
+      }
+    }
+    printf("\x1b[H");
+    for (int k = 0; k < WIDTH * HEIGHT; k++)
+    {
+      cout << ((k % WIDTH) ? buffer[k] : '\n');
+      // putchar(k % WIDTH ? buffer[k] : 10);
+    }
+    A += 0.005;
+    B += 0.005;
+    usleep(1000);
+  }
   return 0;
 }
